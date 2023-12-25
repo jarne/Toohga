ecf31b067a2cf17e7ea09be7af5885cf6925d38f
Jarne <dev@jarne.rocks>
2023-12-25 15:18:41 +0100

    Add first basic Vue client template

---
+++ client/src/main.js
@@ -0,0 +1,29 @@
+/**
+ * Toohga | client main file
+ */
+
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+
+import { Notyf } from "notyf"
+import "notyf/notyf.min.css"
+
+import App from "./App.vue"
+import router from "./router"
+
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "./assets/main.css"
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.provide(
+    "notyf",
+    new Notyf({
+        duration: 5000,
+    })
+)
+
+app.mount("#app")
