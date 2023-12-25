c8bfaf186a2d6152023fc54d5f182acf6d08c383
Jarne <dev@jarne.rocks>
2023-12-25 19:16:46 +0100

    Add CRUD operations in client for URL/user

--- client/src/stores/url.js
+++ client/src/stores/url.js
@@ -21,7 +21,7 @@ export const useUrlStore = defineStore("url", {
                     {
                         method: "GET",
                         headers: {
-                            Authorization: `Bearer ${user.token}`,
+                            Authorization: `Bearer ${auth.token}`,
                         },
                     }
                 )
@@ -34,7 +34,59 @@ export const useUrlStore = defineStore("url", {
                 return
             }
 
-            this.urls = res.urls
+            this.urls = res.short_urls
+        },
+        async deleteUrl(id) {
+            const auth = useAuthStore()
+
+            const resp = await fetch(
+                `${
+                    import.meta.env.VITE_API_ENDPOINT || "/admin/api"
+                }/url/${id}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        Authorization: `Bearer ${auth.token}`,
+                    },
+                }
+            )
+
+            if (resp.status === 204) {
+                await this.loadUrls()
+
+                return
+            }
+
+            const res = await resp.json()
+            if (res.error) {
+                throw new Error(res.error.code)
+            }
+        },
+        async cleanUpUrls() {
+            const auth = useAuthStore()
+
+            const resp = await fetch(
+                `${
+                    import.meta.env.VITE_API_ENDPOINT || "/admin/api"
+                }/urlCleanup`,
+                {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${auth.token}`,
+                    },
+                }
+            )
+
+            if (resp.status === 204) {
+                await this.loadUrls()
+
+                return
+            }
+
+            const res = await resp.json()
+            if (res.error) {
+                throw new Error(res.error.code)
+            }
         },
     },
 })
--- client/src/stores/user.js
+++ client/src/stores/user.js
@@ -1,3 +1,95 @@
 /**
  * Toohga client | user store
  */
+
+import { defineStore } from "pinia"
+
+import { useAuthStore } from "./auth.js"
+
+export const useUserStore = defineStore("user", {
+    state: () => ({
+        users: [],
+    }),
+    actions: {
+        async loadUsers() {
+            const auth = useAuthStore()
+
+            let res
+            try {
+                const resp = await fetch(
+                    `${import.meta.env.VITE_API_ENDPOINT || "/admin/api"}/user`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Authorization: `Bearer ${auth.token}`,
+                        },
+                    }
+                )
+                res = await resp.json()
+            } catch (e) {
+                return
+            }
+
+            if (res.error) {
+                return
+            }
+
+            this.users = res.users
+        },
+        async createUser(uPin, displayName) {
+            const auth = useAuthStore()
+
+            const resp = await fetch(
+                `${import.meta.env.VITE_API_ENDPOINT || "/admin/api"}/user`,
+                {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${auth.token}`,
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        uniquePin: uPin,
+                        displayName: displayName,
+                    }),
+                }
+            )
+
+            if (resp.status === 204) {
+                await this.loadUsers()
+
+                return
+            }
+
+            const res = await resp.json()
+            if (res.error) {
+                throw new Error(res.error.code)
+            }
+        },
+        async deleteUser(id) {
+            const auth = useAuthStore()
+
+            const resp = await fetch(
+                `${
+                    import.meta.env.VITE_API_ENDPOINT || "/admin/api"
+                }/user/${id}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        Authorization: `Bearer ${auth.token}`,
+                    },
+                }
+            )
+
+            if (resp.status === 204) {
+                await this.loadUsers()
+
+                return
+            }
+
+            const res = await resp.json()
+            if (res.error) {
+                throw new Error(res.error.code)
+            }
+        },
+    },
+})
