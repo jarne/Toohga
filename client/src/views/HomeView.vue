4108c10c033fbc8e1d028ea3509e9ee28ce0faa3
Jarne <dev@jarne.rocks>
2025-03-12 15:59:40 +0100

    Fix privacy link not using URL from env variable

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -164,7 +164,7 @@ export default {
                             }}</a>
                         </span>
                         <span v-if="privacyUrl">
-                            | <a href="/privacy">privacy policy</a>
+                            | <a :href="privacyUrl">privacy policy</a>
                         </span>
                     </p>
                 </div>
